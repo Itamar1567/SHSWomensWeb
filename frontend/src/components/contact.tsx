248f9be5592f13a49d2280b8cf601e7fc5f03210
Itamar1567 <Itamarbaharav@gmail.com>
2026-01-27 00:48:13 -0500

    Completed the contact form GUI, TBD: functionality for contact form

--- frontend/src/components/contact.tsx
+++ frontend/src/components/contact.tsx
@@ -1,15 +1,59 @@
-import "./contact.css"
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
+import "./contact.css";
+import { Button } from "@mui/material";
 
-function Contact(){
-    return (<div className="contact-container container">
-        <section className="contact-section">
-            <p className="section-title">Contact Us</p>
-            <div className="contact-information">
-                <p>Phone: (973)-1111-111</p>
-                <p>Email: Example@gmail.com</p>
-            </div>
-        </section>
-    </div>)
+function Contact() {
+  return (
+    <div className="contact-container container">
+      <p className="section-title">Contact Us</p>
+      <section className="contact-section">
+        <div className="contact-information">
+          <div className="contact-form-container">
+            <h1>Send Us a Message</h1>
+            <p>Please fill out the form below to get in touch with us</p>
+            <form className="contact-form">
+              <div className="input-field-pair">
+                <input
+                  placeholder="First name"
+                  type="text"
+                  className="contact-form-input"
+                />
+                <input
+                  placeholder="Last name"
+                  type="text"
+                  className="contact-form-input"
+                />
+              </div>
+              <div className="input-field-pair">
+                <input
+                  placeholder="Email"
+                  type="email"
+                  className="contact-form-input"
+                />
+                <input
+                  placeholder="Phone number"
+                  type="tel"
+                  className="contact-form-input"
+                />
+              </div>
+
+              <textarea id="message-input" placeholder="Message" />
+              <Button type="submit" variant="contained">Send</Button>
+            </form>
+          </div>
+          <div className="micro-contact-info">
+            <PhoneIcon></PhoneIcon>
+            <p>973-1111-111</p>
+          </div>
+          <div className="micro-contact-info">
+            <EmailIcon></EmailIcon>
+            <p>Example@gmail.com</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Contact;
