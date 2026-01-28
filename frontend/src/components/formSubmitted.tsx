fa2060372c1ce309044a71f7ef665ee8515f3cc5
Itamar1567 <Itamarbaharav@gmail.com>
2026-01-27 19:17:50 -0500

    Fixed an issue: Redirect was empty when deployed to prod, Added some styling to the success page

--- frontend/src/components/formSubmitted.tsx
+++ frontend/src/components/formSubmitted.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
-
+import "./formSubmitted.css"
 function FormSubmitted() {
   return (
-    <div className="container">
-      <div>
+    <div className="container submitted-form-container">
+      <div className="submission-message-content">
         <h3>Message Revieved!</h3>
         <p>We will try to respond as quickly as possible</p>
         <Button
@@ -13,7 +13,7 @@ function FormSubmitted() {
           variant="contained"
           fullWidth
         >
-          Return to the Contact Us Page
+          Return to the previous page
         </Button>
       </div>
     </div>
