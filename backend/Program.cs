eeac1f63f5c3ff4ad05c5d5ba06536f6281754bb
Itamar1567 <itamarbaharav@gmail.com>
2026-04-06 16:16:49 -0400

    Add dev and prod enviroments

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,19 @@
+var corsPolicy = "_myPolicy";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
+var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>(); 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(corsPolicy, corsPolicy =>
+    {
+        //Compound assignment, if allowedOrigins null then make it [""] else keep it as is
+        allowedOrigins ??= ["http://localhost:5173/"];
+        corsPolicy.WithOrigins(allowedOrigins).AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
@@ -13,7 +24,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseAuthorization();
 
