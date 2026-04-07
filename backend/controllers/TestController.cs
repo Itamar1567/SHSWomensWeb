feae01c7dcb303051ec1fba71d1de0e2d1d0d033
Itamar1567 <itamarbaharav@gmail.com>
2026-04-07 10:47:59 -0400

    Establish database connection

--- backend/controllers/TestController.cs
+++ backend/controllers/TestController.cs
@@ -6,9 +6,30 @@ using Microsoft.AspNetCore.Mvc;
 [Route("api/[controller]")]
 public class TestController : ControllerBase
 {
-    [HttpGet]
-    public ActionResult Get()
+    private DatabaseRepository _db;
+
+    public TestController(DatabaseRepository db)
+    {
+        _db = db;
+    }
+
+
+    [HttpGet("hello")]
+    public ActionResult GetHello()
     {
         return Ok(new {message = "Hello World"});
+    }
+
+    [HttpGet("database")]
+    public ActionResult GetDatabase()
+    {
+        if (_db.TestConnection())
+        {
+            return Ok(new {message = "Database connection successful"});
+        }
+        else
+        {
+            return StatusCode(500, new {message = "Database connection failed"});
+        }
     }
 }
