68ac792bcf9e352384cf7494393f3073001b5cd5
Itamar1567 <itamarbaharav@gmail.com>
2026-04-07 20:34:13 -0400

    Add Get And Post endpoints for database access and manipulation, create 'interface' class to return newsletter objects

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0755b0665b5f97dac8bca0914610f288e2a8faac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fea5bf5378ff36031f4fd6194a970ab6677e695a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
