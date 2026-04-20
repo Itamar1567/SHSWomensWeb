b83149405b1505e3aa844dbd7e6acd53e0b6ec21
Itamar1567 <Itamarbaharav@gmail.com>
2026-04-20 16:25:11 -0400

    Add website redeployment ability

---
+++ backend/controllers/NetlifyController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/{controller}")]
+public class NetlifyController : ControllerBase
+{
+    FrontendActions _frontendActions;
+    public NetlifyController(FrontendActions frontendActions)
+    {
+        _frontendActions = frontendActions;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> RedeployNetlifyWebsite()
+    {
+        try
+        {
+            await _frontendActions.RedeployMainWeb();
+            return Ok(new {message="Changes have been commited"});
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Unable to redeploy", ex);
+            return StatusCode(500, new {message="Unable to redeploy website", error=ex});
+        }
+    }
+}
--- backend/utilities/FrontendActions.cs
+++ backend/utilities/FrontendActions.cs
@@ -9,18 +9,17 @@ public class FrontendActions
         _client = client;
     }
 
-    public async Task<bool> RedeployMainWeb()
+    public async Task RedeployMainWeb()
     {
         try
         {
             var res = await _client.PostAsync(Environment.GetEnvironmentVariable("NetlifyBuildAPIKey"), null);
             Console.WriteLine("Redeploy response: " + res.StatusCode);
-            return true;
         }
         catch (Exception ex)
         {
             Console.WriteLine("Error occurred while redeploying main web: " + ex.Message);
-            return false;
+            throw new Exception("Error occurred while redeploying main web");
         }
     }
 }
