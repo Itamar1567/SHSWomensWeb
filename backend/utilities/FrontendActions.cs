ef9e41e40b8993033b2bb67b59b86313e3c7a841
Itamar1567 <Itamarbaharav@gmail.com>
2026-06-04 14:26:34 -0400

    Add new checkers for main website redeploy function

--- backend/utilities/FrontendActions.cs
+++ backend/utilities/FrontendActions.cs
@@ -10,16 +10,22 @@ public class FrontendActions
     }
 
     public async Task RedeployMainWeb()
+{
+    var url = Environment.GetEnvironmentVariable("NetlifyBuildAPI");
+
+    //If null the variable was not found
+    if (string.IsNullOrEmpty(url))
+        throw new Exception("NetlifyBuildAPI environment variable is not set");
+
+    try
     {
-        try
-        {
-            var res = await _client.PostAsync(Environment.GetEnvironmentVariable("NetlifyBuildAPI"), null);
-            Console.WriteLine("Redeploy response: " + res.StatusCode);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Error occurred while redeploying main web: " + ex.Message);
-            throw new Exception("Error occurred while redeploying main web");
-        }
+        var res = await _client.PostAsync(url, null);
+        Console.WriteLine("Redeploy response: " + res.StatusCode);
     }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error occurred while redeploying main web: " + ex);
+        throw new Exception("Error occurred while redeploying main web");
+    }
+}
 }
