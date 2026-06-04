2f56286de1b5879f580442f4b5b6a0fa83400c72
Itamar1567 <Itamarbaharav@gmail.com>
2026-06-04 13:05:29 -0400

    Add automatic migration appliance

--- backend/Program.cs
+++ backend/Program.cs
@@ -44,6 +44,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
+    Console.WriteLine("Using connection string: " + connectionString);
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
