c64671d60ab895345ba69326475d6f57ec99b22d
Itamar1567 <Itamarbaharav@gmail.com>
2026-06-07 12:33:40 -0400

    add privacy-policy

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -7,21 +7,34 @@ function Footer() {
   return (
     <div className="footer-container container">
       <section className="extra-information-section">
-          <div>
-            <p className="footerText">Follow Us</p>
-            <section className="icons-section">
-              <a href="https://www.instagram.com/" target="_blank"><InstagramIcon className="follow-icon"></InstagramIcon></a>
-              <a href="https://x.com/" target="_blank"><XIcon className="follow-icon"></XIcon></a>
-            </section>
-          </div>
-          <div className="extra-nav">
-            <Link to="/" className="basic-link">About Us</Link>
-            <Link to="/newsletter" className="basic-link">Newsletter</Link>
-            <Link to="/contact" className="basic-link">Contact</Link>
-          </div>
-          <div>
-            <p className="footerText">SHS Women's Health Club</p>
-          </div>
+        <div>
+          <p className="footerText">Follow Us</p>
+          <section className="icons-section">
+            <a href="https://www.instagram.com/" target="_blank">
+              <InstagramIcon className="follow-icon"></InstagramIcon>
+            </a>
+            <a href="https://x.com/" target="_blank">
+              <XIcon className="follow-icon"></XIcon>
+            </a>
+          </section>
+        </div>
+        <div className="extra-nav">
+          <Link to="/" className="basic-link">
+            About Us
+          </Link>
+          <Link to="/newsletter" className="basic-link">
+            Newsletter
+          </Link>
+          <Link to="/contact" className="basic-link">
+            Contact
+          </Link>
+        </div>
+        <div>
+          <p className="footerText">SHS Women's Health Club</p>
+          <a href="/privacyPolicy.html" target="_blank" className="basic-link">
+            Privacy Policy
+          </a>
+        </div>
       </section>
     </div>
   );
