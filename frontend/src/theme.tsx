675c1a2780350d011087e983133703fcf07f881e
Itamar1567 <Itamarbaharav@gmail.com>
2026-06-09 18:37:22 -0400

    Change contact form look and main button color

--- frontend/src/theme.tsx
+++ frontend/src/theme.tsx
@@ -6,7 +6,7 @@ const theme = createTheme({
   },
   palette: {
     primary: {
-      main: "#fdc5ff",
+      main: "#fdb5ff",
       light: "#ffb8f9",
       dark: "rgb(255, 135, 255)",
       contrastText: "#242105",
